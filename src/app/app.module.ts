e23d1cce5d93ad87fb111f2e972f4bce5d710441
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-04-24 17:19:09 +0200

    24-4-23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
+//librerias de módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { AboutComponent } from './shared/about/about.component';
+import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { TerminosComponent } from './pages/terminos/terminos.component';
+import { ItemComponent } from './pages/item/item.component';
 
+//Decorador de módulos
 @NgModule({
+  //Componentes
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     AboutComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    TerminosComponent,
+    ItemComponent
+    
   ],
   imports: [
     BrowserModule
